920207cd5ce9644560f5aa3e238fa9873faa05cc
Franck-Dev <f.dartois@daher.com>
2021-01-27 00:54:41 +0100

    Creation des tests php

---
+++ tests/Controller/TricksControllerTests.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\Tricks;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TricksControllerTests extends WebTestCase
+{    
+    /**
+     * testricksNew Test de la fonction de création de Tricks
+     * Renvoie une redirection temporaire
+     *
+     * @return void
+     */
+    public function testricksNew()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/tricks/new');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        // $this->assertResponseRedirects('/tricks/new.html.twig');
+    }
+    
+    /**
+     * testricksIndex Test fonction index des Tricks
+     *
+     * @return void
+     */
+    public function testricksIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/tricks');
+
+        $this->assertEquals(301, $client->getResponse()->getStatusCode());
+    }
+}
